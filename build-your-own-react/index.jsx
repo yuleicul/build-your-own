4c4dcf68a64ff7dcacd444393a35464104e89729
Yulei Chen <yulei_chen@epam.com>
2022-09-20 12:09:51 +0800

    First version with one node

--- build-your-own-react/index.jsx
+++ build-your-own-react/index.jsx
@@ -1,3 +1,25 @@
-const element = <h1 title="foo">Hello</h1>
-const container = document.getElementById('root')
-ReactDOM.render(element, container)
+const React = {
+  createElement(...args) {
+    const [tagName, props, children] = args;
+
+    const newElement = document.createElement(tagName);
+
+    for (const prop in props) {
+      newElement.setAttribute(prop, props[prop]);
+    }
+
+    if (typeof children === "string" || typeof children === "number") {
+      const textNode = document.createTextNode(children);
+      newElement.appendChild(textNode);
+    } else {
+      newElement.appendChild(children);
+    }
+
+    return newElement;
+  },
+};
+
+const element = <h1 title="foo">Hello</h1>;
+const container = document.getElementById("root");
+// ReactDOM.render(element, container);
+container.appendChild(element);
