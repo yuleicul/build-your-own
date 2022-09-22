4937888c8d14602b675661d6b7b7a47a478d92a9
Yulei Chen <yulei_chen@epam.com>
2022-09-22 21:59:12 +0800

    reat: perform unit of work (fiber)

--- build-your-own-react/README.md
+++ build-your-own-react/README.md
@@ -1,1 +1,6 @@
 https://babeljs.io/repl
+
+https://stackoverflow.com/questions/29285897/what-is-the-difference-between-for-in-and-for-of-statements
+
+> key in (index of array and key of object are both **key**)
+> value of
--- build-your-own-react/index.jsx
+++ build-your-own-react/index.jsx
@@ -1,24 +1,139 @@
 const React = {
   createElement(...args) {
     const [tagName, props, ...children] = args;
-    console.log(children);
+    return {
+      type: tagName,
+      props: {
+        ...props,
+        children,
+      },
+    };
+  },
+};
 
-    const newElement = document.createElement(tagName);
+/**
+ * Fiber {
+ *   dom, // only this property is dom!
+ *   type,
+ *   props {
+ *     children
+ *   },
+ *   child,
+ *   nextSibling,
+ *   parent
+ * }
+ *
+ * Virtual Dom is inside Fiber.
+ */
 
-    for (const prop in props) {
-      newElement.setAttribute(prop, props[prop]);
+const ReactDOM = {
+  TEXT_OR_NUMBER: "TEXT_OR_NUMBER",
+  // virtualDom is inside fiber
+  createDom(fiber) {
+    const { type, props } = fiber;
+
+    if (type === ReactDOM.TEXT_OR_NUMBER) {
+      return document.createTextNode(fiber.props);
+    } else {
+      const element = document.createElement(type);
+
+      for (const prop in props) {
+        if (prop !== "children") {
+          element.setAttribute(prop, props[prop]);
+        }
+      }
+      return element;
     }
+  },
 
-    for (const child of children) {
-      if (typeof child === "string" || typeof child === "number") {
-        const textNode = document.createTextNode(child);
-        newElement.appendChild(textNode);
-      } else {
-        newElement.appendChild(child);
+  /**
+   * unitOfWork = fiber = {
+   *    dom,
+   *    type,
+   *    props: { children },
+   *    parent,
+   *    nextSibling
+   * }
+   *
+   * What does this function do?
+   * 1. render the current dom
+   * 2. prepare the next run fiber and return it
+   *
+   * The main logic inside performUnitWork is to transform Virtual Dom to Fiber, and construct the Fiber Tree. Just add some `point`s, take it easy
+   * (but I'm not sure if it's called virtual dom)
+   */
+  performUnitOfWork(fiber) {
+    console.log(fiber);
+    if (!fiber.dom) fiber.dom = this.createDom(fiber);
+
+    // main task: render the current child to parent!
+    if (fiber.parent) fiber.parent.dom.appendChild(fiber.dom);
+
+    // prepare for the next run:
+    // main task is to link all children one by one, at the same time link them to the same parent
+    let firstChild;
+    const children = fiber.props.children;
+    if (children) {
+      for (let i = 0; i < children.length; i++) {
+        if (
+          typeof children[i] === "string" ||
+          typeof children[i] === "number"
+        ) {
+          const temp = children[i];
+          children[i] = {
+            type: ReactDOM.TEXT_OR_NUMBER,
+            props: temp,
+          };
+        }
+
+        if (i === 0) firstChild = children[i];
+
+        children[i].parent = fiber;
+
+        if (i + 1 < children.length) {
+          children[i].nextSibling = children[i + 1];
+        }
       }
     }
 
-    return newElement;
+    if (firstChild) {
+      return firstChild;
+    }
+
+    // HACK: literate version of tree's traverse
+    let currentFiber = fiber;
+    while (currentFiber) {
+      if (currentFiber.nextSibling) {
+        return currentFiber.nextSibling;
+      }
+      currentFiber = currentFiber.parent;
+    }
+  },
+
+  workLoop(deadline) {
+    console.log("deadline.timeRemaining", deadline.timeRemaining());
+
+    while (deadline.timeRemaining() >= 1 && ReactDOM.nextUnitOfWork) {
+      ReactDOM.nextUnitOfWork = ReactDOM.performUnitOfWork(
+        ReactDOM.nextUnitOfWork
+      );
+    }
+
+    if (ReactDOM.nextUnitOfWork) window.requestIdleCallback(ReactDOM.workLoop);
+  },
+
+  render(virtualDom, container) {
+    ReactDOM.nextUnitOfWork = {
+      dom: container,
+      props: {
+        children: [virtualDom],
+      },
+      // parent, nextSibling are all `point` to `fiber`
+      parent: null,
+      nextSibling: null,
+    };
+
+    window.requestIdleCallback(this.workLoop);
   },
 };
 
@@ -27,8 +142,8 @@ const element = (
     <a>bar</a>
     <b />
     <p>hello world</p>
+    <h1>react</h1>
   </div>
 );
 const container = document.getElementById("root");
-// ReactDOM.render(element, container);
-container.appendChild(element);
+ReactDOM.render(element, container);
