d19e3f9bd8338eca4bfd979e2d59fdf69c1970b2
Yulei Chen <yulei_chen@epam.com>
2022-11-24 18:13:46 +0800

    Build your own global state manager

--- build-your-own-global-state-manager/src/App.jsx
+++ build-your-own-global-state-manager/src/App.jsx
@@ -1,34 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const createGlobalState = (initialState) => {
+  const globalState = initialState;
+  const setters = {};
 
+  const useGlobalState = (key) => {
+    const [state, setState] = useState(globalState[key]);
+
+    useEffect(() => {
+      if (setters[key]) setters[key].add(setState);
+      else setters[key] = new Set();
+      return () => {
+        setters[key].delete(setState);
+      };
+    }, []);
+
+    const setGlobalState = (state) => {
+      globalState[key] = state;
+      for (const setter of setters[key]) {
+        setter(state);
+      }
+      console.log(globalState, setters);
+    };
+
+    return [state, setGlobalState];
+  };
+
+  return {
+    useGlobalState,
+  };
+};
+
+const initialState = {
+  count: 0,
+  text: "hello",
+};
+const { useGlobalState } = createGlobalState(initialState);
+
+const Counter = () => {
+  const [value, update] = useGlobalState("count");
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <span>Count: {value}</span>
+      <button type="button" onClick={() => update(value + 1)}>
+        +1
+      </button>
+      <button type="button" onClick={() => update(value - 1)}>
+        -1
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const TextBox = () => {
+  const [value, update] = useGlobalState("text");
+  return (
+    <div>
+      <span>Text: {value}</span>
+      <input value={value} onChange={(event) => update(event.target.value)} />
+    </div>
+  );
+};
+
+const App = () => (
+  <>
+    <h1>Counter</h1>
+    <Counter />
+    <Counter />
+    <h1>TextBox</h1>
+    <TextBox />
+    <TextBox />
+  </>
+);
+
+export default App;
